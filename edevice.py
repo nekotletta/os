a888693aa9bff67a2dca87a175bb79fb00f6c09b
nekotletta <99048617+nekotletta@users.noreply.github.com>
2023-10-02 12:23:21 -0400

    update edevice.py

--- edevice.py
+++ edevice.py
@@ -1,20 +1,28 @@
 import socket
-import sys 
+import sys
 import random
-import time 
+import time
+import threading 
 
-for _ in range(5):
+#EL BONO SE IMPLEMENTO USANDO FUNCIONES PARA QUE FUERA FACIL
+#CORRER EL PROGRAMA SIN EL BONO POR SI CAUSABA PROBLEMAS
+#QUE NO SE DAÑARA NADA QUE YA ESTUVIERA CPMPLETADO
+bufferSize = 10000
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+def ejecutar():
+  global bufferSize
+  msg = ''
+  MsgFromServer = UDPClientSocket.recvfrom(bufferSize)
+  msg = MsgFromServer[0]
+  print(msg)
+    
+for _ in range(5): 
   id = str(sys.argv[1])
   job_time = random.randint(1, 3)
   id_time = (str(id) + ":" + str(job_time))
   print(id_time)
   bytesToSend = str.encode(id_time)
-  serverAddressPort = (sys.argv[2], int(sys.argv[3])) 
-  
-  bufferSize = 1000
-  
-  UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+  serverAddressPort = (sys.argv[2], int(sys.argv[3]))
 
   UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-
-  time.sleep(random.randint(1,3))
+  ejecutar()
